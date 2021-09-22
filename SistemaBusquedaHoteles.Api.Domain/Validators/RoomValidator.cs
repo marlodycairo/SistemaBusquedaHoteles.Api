f8c5b650ce67ed05d37cd4519111ead58c8d449b
Marlody <marlodycairo@hotmail.com>
2021-09-21 21:11:13 -0500

    Actualizacion

---
+++ SistemaBusquedaHoteles.Api.Domain/Validators/RoomValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using SistemaBusquedaHoteles.Api.Domain.Models;
+
+namespace SistemaBusquedaHoteles.Api.Domain.Validators
+{
+    public class RoomValidator : AbstractValidator<RoomModel>
+    {
+        public RoomValidator()
+        {
+            RuleFor(p => p.)
+        }
+    }
+}
